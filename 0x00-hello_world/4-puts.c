7db1b93393d3e489e102be3c3113f28df25856e3
Light <fatoki.paul23@gmail.com>
2023-07-13 17:07:17 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* main - a program that prints a msg
+ *
+ * Return - Always (0) success
+ *
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
