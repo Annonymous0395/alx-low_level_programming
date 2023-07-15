59400c7dccf9932239a4ee3daf19197f2ec9cff3
Light <fatoki.paul23@gmail.com>
2023-07-14 20:14:50 -0700

    numberz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a = 'z';
+
+	for (a = 'z'; a <= 'a'; a--)
+
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
