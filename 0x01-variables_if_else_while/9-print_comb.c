6c49c834e0867a6f2d895934703ee7295a3f3f5a
Light <fatoki.paul23@gmail.com>
2023-07-16 11:52:57 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,14 @@ int main(void)
 {
 	int a = '0';
 
-	for (a = '0'; a < '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
-		putchar(',');
-		putchar(' ');
+		if (a < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('9');
 	return (0);
