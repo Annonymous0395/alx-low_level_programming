1a64840cf41c06c561c86c40647d4149a4955573
Light <fatoki.paul23@gmail.com>
2023-07-16 16:36:22 -0700

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,10 @@
 int main(void)
 {
 	int a = '0';
-	int b = '1';
 
 	for (a = '0'; a < '9'; a++)
-	{
-		for (b = '1'; b <= '9'; b++)
+	{	int b = a + 1;
+		for (; b <= '9'; b++)
 		{
 			putchar(a);
 			putchar(b);
