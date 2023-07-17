d6388c6da8bc41e8600bdcc6c4868d40c725e536
Light <fatoki.paul23@gmail.com>
2023-07-16 19:26:09 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+	char b = 'B';
+
+	for (char a = 'a'; a <= 'z'; a = a + 2)
+	{
+		putchar(a);
+		putchar(b);
+		b = b + 2;
+	}
+	putchar('\n');
+	return (0);
+}
