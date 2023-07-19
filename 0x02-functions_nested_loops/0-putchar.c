b9b3854d32184bb7cb68adaeebcdee49ca8a34df
Light <fatoki.paul23@gmail.com>
2023-07-18 17:22:45 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	purchar('r');
 	return (0);
 }
