3bb2b3cb1d3e1287c5b86b7327e171749f973609
Light <fatoki.paul23@gmail.com>
2023-07-24 23:45:18 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - Entry point
+ * @n - number to check
  *
  * Return: 1 if positive. 0 if zero. -1 if negative
  */
@@ -13,7 +14,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
