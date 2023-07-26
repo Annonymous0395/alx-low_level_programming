7b94efb116e1ff647fd09d25750fbc121b8725df
Light <fatoki.paul23@gmail.com>
2023-07-26 10:36:28 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: to be added
+ * @b: to be added
+ *
+ * Return: value n (success)
+ */
+
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return (n);
+}
