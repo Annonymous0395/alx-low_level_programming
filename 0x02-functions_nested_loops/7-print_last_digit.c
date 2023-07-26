6de5cfae4f930c7a6dd32a76b048aa702a9c1c95
Light <fatoki.paul23@gmail.com>
2023-07-25 17:26:41 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Entry point
- * @int: data of the function
+ * @c: data of the function
  *
  * Return: value of c  (success)
  */
@@ -12,7 +12,6 @@ int print_last_digit(int c)
 	int n = c % 10;
 
 	_putchar(n);
-
 	return (n);
 }
 
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == 2 && j == 4)
+			{
+				break;
+			}
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
