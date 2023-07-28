575618f836a477da2f8bb160aa34d14f3d22c9be
Light <fatoki.paul23@gmail.com>
2023-07-28 15:39:40 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,12 @@
 void print_rev(char *s)
 {
 	int counter = 0;
-	int i;
 
 	while (s[counter] != '\0')
 	{
 		counter++;
-		i = counter;
 	}
-	while (counter != 0)
+	while (counter >= 0)
 	{
 		char c = s[counter];
 		_putchar(c);
