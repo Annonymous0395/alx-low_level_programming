f763be963850704917df9aaaed478832f58b2f4c
Light <fatoki.paul23@gmail.com>
2023-07-28 15:01:30 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: param to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+int _putchar (char s)
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif
