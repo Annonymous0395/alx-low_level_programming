40e7ef614ebff2bb81d2842471e193602e827d90
Light <fatoki.paul23@gmail.com>
2023-07-28 11:48:23 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: param to be checked
+ *
+ * Return: Always 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < *s[]; i++)
+	{
+		int counter = i;
+	}
+	return (counter);
+}	
