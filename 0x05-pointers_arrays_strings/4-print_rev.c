e4ed66bd835fdfa0f730b722cefc1b2a08e30f10
Light <fatoki.paul23@gmail.com>
2023-07-28 15:33:56 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: param to be printed backwards
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	counter = counter - 1;
+	while (counter == 0)
+	{
+		char c = s[counter];
+		_putchar(c);
+		counter--;
+	}
+}	
