8ed449690eb59371083ef3940cd636322d38ddf0
Light <fatoki.paul23@gmail.com>
2023-07-28 11:31:00 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: first param
+ * @b: second param
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = a;
+	int d = b;
+
+	*a = c;
+	*b = d;
+}
