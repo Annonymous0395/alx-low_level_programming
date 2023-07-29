1155e21fc0d38979089a0710966301e598910e15
Light <fatoki.paul23@gmail.com>
2023-07-28 17:55:34 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: param to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int counter = 0;
+	char c = char *s
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	counter = counter - 1;
+	for (i = 0; i <= counter; i++)
+	{
+		s[i] = c[counter];
+		counter--;
+	}
+}
