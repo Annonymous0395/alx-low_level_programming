dbbd9a07d439f70e7ed54bd497f47036d4df4225
Light <fatoki.paul23@gmail.com>
2023-07-28 19:26:28 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: param to be printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+
+	counter = counter - 1;
+	while (i < counter)
+	{
+		if (i % 2 == 0)
+		{
+			char c = str[i];
+		}
+		i++;
+	}
+}
