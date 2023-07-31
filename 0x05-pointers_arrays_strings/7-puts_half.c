d70c30a6242b59dd57edf88907a652754e645f0e
Light <fatoki.paul23@gmail.com>
2023-07-31 13:29:43 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * @str: param to be cut
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+	if (counter % 2 == 0)
+	{
+		int n = counter / 2;
+
+		while (n != counter)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+		else
+		{
+			n = (counter - 1) / 2;
+			while (n != counter)
+			{
+				_putchar(str[n]);
+				n++;
+			}
+		}
+
