dd5aba4367442552a82f5b0a2b9b964eba91117f
Light <fatoki.paul23@gmail.com>
2023-08-02 12:49:34 -0700

    void _puts_recursion(char *s);

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry point
+ * @s: param to be printed
+ *
+ * Return: void
+ */
+
+void _puts_recursion (char *s)
+{
+	int n = 0;
+
+	if (s[n] != '\0')
+	{
+		char r = s[n];
+
+		_putchar(r);
+		n++;
+		_puts_recursion(s[n]);
