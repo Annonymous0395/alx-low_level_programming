c58a0189c335c630e51e7dca05c9fbf5250fc825
Light <fatoki.paul23@gmail.com>
2023-08-03 12:59:19 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+void _putsc(char *s, int n)
 /**
  * _puts_recursion - Entry point
  * @s: param to be printed
@@ -9,9 +10,28 @@
 
 void _puts_recursion(char *s)
 {
-	if (s)
+	int n = 0;
+
+	if (s[n] != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(s[n]);
+		_putsc(s, n + 1);
+	}
+}
+
+/**
+ * _putsc - Entry point
+ * @s: param to be printed
+ * @n: param to be cross-refrenced
+ *
+ * Return: void
+ */
+
+void _putsc(char *s, int n)
+{
+	if(s[n] != '\0')
+	{
+		_putchar(s[n]);
+		_putsc(s, n + 1);
 	}
 }
