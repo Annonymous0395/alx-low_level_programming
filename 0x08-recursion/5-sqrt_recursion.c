b41acd7f46961ee79f4a5e2c6483154adf0ea25d
Light <fatoki.paul23@gmail.com>
2023-08-02 20:13:41 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int _sqrt(int i, int n);
+
+/**
+ * _sqrt_recursion - Entry point
+ * @n: param to be sqrt
+ *
+ * Return: -1 (error)
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt(i, n));
+	}
+}
+
+/**
+ * _sqrt - Entry point
+ * @i: param to be multiplied
+ * @n: param to be sqrt
+ *
+ * Return: i if successful
+ */
+
+int _sqrt(int i, int n)
+{
+	if (i > n/2)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (i * i == n)
+		{
+			return (i);
+		}
+		return (_sqrt(i++, n));
+	}
+}
