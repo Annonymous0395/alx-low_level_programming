cc3739302140c5e254a6f04bb831511c4967a40f
Light <fatoki.paul23@gmail.com>
2023-08-04 19:56:57 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	argc = 0;
 	return (0);
 }
