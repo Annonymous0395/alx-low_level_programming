c598eca446ad746a3fcbc38ea0c4bad9a68e82ec
Light <fatoki.paul23@gmail.com>
2023-08-04 19:44:09 -0700

    fix

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: command line
+ * @argv: array of argc
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILR
+
+int _putchar(char c);
+
+#endif
