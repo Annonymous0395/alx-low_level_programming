1d5a5382bda32e245558d711a438e7d9524e1d73
Light <fatoki.paul23@gmail.com>
2023-08-24 18:01:12 -0700

    fix

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * print_lis - Entry point
+ * @h: param meter that points to list_t
+ *
+ * Return: the number of nodes (success)
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+
+	return (n);
