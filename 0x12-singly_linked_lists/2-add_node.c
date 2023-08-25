3c9c52dded1bc1ffa169918cea37f7d63ed19ab0
Light <fatoki.paul23@gmail.com>
2023-08-24 18:36:50 -0700

    fix

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - Entry point
+ * @head: param that points to the list_t 
+ * @str: param of type string to be added into the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *forward;
+	unsigned int n = 0;
+
+	while (str[n])
+	{
+		n++;
+	}
+	forward = malloc(sizeof(list_t));
+	if (!forward)
+	{
+		return (NULL);
+	}
+	forward->str = strdup(str);
+	forward->n = n;
+	forward->next = (*head);
+	(*head) = forward;
+
+	return (*head);
+}
