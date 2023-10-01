11b5737f3981f7f96e95a367f63c779504c1777e
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-01 14:44:30 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,24 +14,24 @@ void _puts_recursion(char *s)
 	if (s[n] != '\0')
 	{
 		_putchar(s[n]);
-		_putn(s, n + 1);
+		_putsn(s, n + 1);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
 
 /**
- * _putn - Entry point
+ * _putsn - Entry point
  * @s: param to be printed
  * @n: counter for param
  *
  * Return: void
  */
 
-void _putn(char *s, int n)
+void _putsn(char *s, int n)
 {
 	if (s[n] != '\0')
 	{
 		_putchar(s[n]);
-		_putn(s, n + 1);
+		_putsn(s, n + 1);
 	}
 }
