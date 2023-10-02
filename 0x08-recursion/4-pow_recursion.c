6694bcc18e362b35d906ca6670e218190d5c217b
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-01 17:04:45 -0700

    recursion2

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @j: param multiple
+ * @k: used multiplier
+ *
+ * Return: -1 (error)
+ */
+
+int _pow_recursion(int j, int k)
+{
+	if (j < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (k == 0)
+		{
+			return (1);
+		}
+		return (j * _pow_recursion(j, k - 1));
+	}
+}
