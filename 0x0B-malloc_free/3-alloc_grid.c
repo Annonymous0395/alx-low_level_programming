139766abf543a3c9f6ad76b15768f72f956f7eeb
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-05 18:12:55 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Entry point
+ * @width: param of width
+ * @height: param for height
+ * Return: value to 2 places
+ */
+int **alloc_grid(int width, int height)
+{
+        int *value;
+        int w, h;
+
+        if (width <= 0 || height <= 0)
+                return (NULL);
+
+        value = malloc(sizeof(int *) * height);
+
+        if (value == NULL)
+                return (NULL);
+
+        for (w = 0; w < height; w++)
+        {
+                value[w] = malloc(sizeof(int) * width);
+
+                if (value[w] == NULL)
+                {
+                        for (; w >= 0; w--)
+                                free(value[w]);
+
+                        free(value);
+                        return (NULL);
+                }
+        }
+
+        for (w = 0; w < height; w++)
+        {
+                for (h = 0; h < width; h++)
+                        mee[w][h] = 0;
+        }
+
+        return (value);
+}
