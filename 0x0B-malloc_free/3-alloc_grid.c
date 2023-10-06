46f39286c196f119381892f981fd2b62b4bef27f
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-05 18:27:05 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,36 +8,39 @@
  */
 int **alloc_grid(int width, int height)
 {
-        int **value;
-        int w, h;
+	int **value;
+	int w, h;
 
-        if (width <= 0 || height <= 0)
-                return (NULL);
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	value = malloc(sizeof(int *) * height);
 
-        value = malloc(sizeof(int *) * height);
+	if (value == NULL)
+	{
+		return (NULL);
+	}
+	for (w = 0; w < height; w++)
+	{
+		value[w] = malloc(sizeof(int) * width);
+		if (value[w] == NULL)
+		{
+			for (; w >= 0; w--)
+			{
+				free(value[w]);
+			}
+			free(value);
+			return (NULL);
+		}
+	}
 
-        if (value == NULL)
-                return (NULL);
-
-        for (w = 0; w < height; w++)
-        {
-                value[w] = malloc(sizeof(int) * width);
-
-                if (value[w] == NULL)
-                {
-                        for (; w >= 0; w--)
-                                free(value[w]);
-
-                        free(value);
-                        return (NULL);
-                }
-        }
-
-        for (w = 0; w < height; w++)
-        {
-                for (h = 0; h < width; h++)
-                        value[w][h] = 0;
-        }
-
-        return (value);
+	for (w = 0; w < height; w++)
+	{
+		for (h = 0; h < width; h++)
+		{
+			value[w][h] = 0;
+		}
+	}
+	return (value);
 }
