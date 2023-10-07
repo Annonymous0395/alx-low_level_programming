ecf2180a8e8d3c327f1f2af43585d8ce6ea528ce
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-07 16:13:12 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of command line args
+ * @argv: array of argc
+ *
+ * Return: 0 (successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
