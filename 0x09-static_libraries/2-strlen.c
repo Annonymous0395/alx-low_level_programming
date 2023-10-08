5a0b22914307c8374bc1a10d542463bc233bb484
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-07 18:46:35 -0700

    fix

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,19 @@
-int _strlen(char *s);
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	return (i);
+}
