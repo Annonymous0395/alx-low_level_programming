ddd1b3dc429b16bc02ab8f53aa91cd430c1abc8d
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-09 18:36:44 -0700

    1-string_nocat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,8 +11,10 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int l, k , ls, i;
+	char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -20,31 +22,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[l] != '\0')
-	{
-		l++;
-	}
-	while (s2[k] != '\0')
-	{
-		k++;
-	}
-	if (n > k)
-		n = k;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-	ls = l + n;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-	s = malloc(ls + 1);
+	if (n > ls2)
+		n = ls2;
 
-	if (s == NULL)
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
 
-	for (i = 0; i < ls; i++)
-		if (i < i)
-			s[i] = s1[i];
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
 		else
-			s[i] = s2[i - l];
+			sout[i] = s2[i - ls1];
 
-	s[i] = '\0';
+	sout[i] = '\0';
 
-	return (s);
+	return (sout);
 }
