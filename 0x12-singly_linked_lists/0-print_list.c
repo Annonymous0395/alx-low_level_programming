0dfa067948075fe702613e3ad8c31cc9ef00211a
Annonymous0395 <fatoki.paul23@gmail.com>
2023-10-19 16:56:42 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_list - Entry point
+ * @h: param list
+ *
+ * Return: n if successful
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
